097de9efdcf43d29599393588717cfdc2a9233b9
ashish kori <ashishkumarkori1@gmail.com>
2018-10-11 18:25:59 +0530

    Cloning

--- myJava/src/myJava/StringConstantPool.java
+++ myJava/src/myJava/StringConstantPool.java
@@ -12,14 +12,14 @@ public class StringConstantPool {
 		String s4="You can not change me";
 		System.out.println(s3==s4);//true
 		System.out.println(s1==s3);//false
-		String s5="You can not "+ "change me";//performed at compile time because both are Compile time Constant
+		String s5="You can not "+ "change me";//Performed at compile time because both are Compile time Constant
 		System.out.println(s3==s5);//true
 		String s6="You can not";
-		String s7=s6+" change me";//performed at Runtime  because at least one is normal variable
+		String s7=s6+" change me";//Performed at Runtime  because at least one is normal variable
 		System.out.println(s7==s1);//false
 		System.out.println(s1.equals(s7));//true
 		final String s8="You can not";
-		String s9=s8+" change me"; // Reason:performed at compile time because both are Compile time Constant
+		String s9=s8+" change me"; // Performed at compile time because both are Compile time Constant
 		System.out.println(s3==s9);//true
 		System.out.println(s6==s8);//true
 	}
