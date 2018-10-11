6d1ae8ae3ec6344a0bbaaf33577949b62986f3cf
ashish kori <ashishkumarkori1@gmail.com>
2018-10-11 21:55:05 +0530

    SCP Intern method

---
+++ myJava/src/myJava/InterningStringDemo1.java
@@ -0,0 +1,13 @@
+package myJava;
+
+public class InterningStringDemo1 {
+	public static void main(String[] args) {
+		String s1=new String("Ashish");
+		String s2=s1.concat(" Kumar");
+		//System.out.println(s1==s2);
+		String s3=s2.intern();
+		System.out.println(s1==s3);
+		String s4="Ashish Kumar";
+		System.out.println(s2==s4);
+		}
+}
